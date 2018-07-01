c4d32ad0fd304f14827236e28fd87d242376a4db
Amer Khaldi <khaldi.ameur1995@gmail.com>
2018-07-01 11:10:42 +0100

    Improving sw.

--- app.js
+++ app.js
@@ -189,7 +189,9 @@ function makeRequest () {
 
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
+    navigator.serviceWorker.register('sw.js', {
+        scope: '.'
+    }).then(function(reg) {
 
         if(reg.installing) {
             console.log('Service worker installing');
